006188edce6632f754d6345fcffce8280eb6d106
PatrickAraujoD <135553025+PatrickAraujoD@users.noreply.github.com>
2024-06-08 17:55:12 -0300

    refactor: change id column not autoincrement

--- src/infra/entities/users.py
+++ src/infra/entities/users.py
@@ -6,10 +6,19 @@ from src.infra.config import Base
 class Users(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     idPet = relationship("Pets")
 
     def __rep__(self):
         return f"User [name={self.name}]"
+
+    def __eq__(self, other):
+        if isinstance(other, Users):
+            return (
+                self.id == other.id
+                and self.name == other.name
+                and self.password == other.password
+            )
+        return False
