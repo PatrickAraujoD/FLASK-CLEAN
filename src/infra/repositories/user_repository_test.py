c27a40d4cd03b913a4b06a178fd9891daa08ba1b
PatrickAraujoD <135553025+PatrickAraujoD@users.noreply.github.com>
2024-06-08 17:55:27 -0300

    test: create testSelectUser

--- src/infra/repositories/user_repository_test.py
+++ src/infra/repositories/user_repository_test.py
@@ -1,7 +1,8 @@
 from faker import Faker
+from sqlalchemy import text
 from src.infra.config import BDConnectionHandler
 from .user_repository import UserRepository
-from sqlalchemy import text
+from src.infra.entities import Users as UsersModel
 
 faker = Faker()
 userRepository = UserRepository()
@@ -27,3 +28,33 @@ def testInsertUser():
     assert result is not None
     assert result.name == newUser.name
     assert result.password == newUser.password
+
+
+def testSelectUser():
+    """Should select a user in Users table and compare it"""
+    userId = faker.random_number(digits=5)
+    name = faker.name()
+    password = faker.word()
+    data = UsersModel(id=userId, name=name, password=password)
+
+    engine = connection.getEngine()
+
+    with engine.connect() as conn:
+        query = text(
+            f"INSERT INTO users(id, name, password) VALUES (:id, :name, :password)"
+        )
+        conn.execute(query, {"id": userId, "name": name, "password": password})
+        conn.commit()
+
+    queryUser = userRepository.selectUser(userId=userId)
+    queryUser2 = userRepository.selectUser(name=name)
+    queryUser3 = userRepository.selectUser(userId=userId, name=name)
+    print(data in queryUser)
+    assert data in queryUser
+    assert data in queryUser2
+    assert data in queryUser3
+
+    with engine.connect() as conn:
+        query = text(f"DELETE FROM users WHERE id=:id")
+        conn.execute(query, {"id": userId})
+        conn.commit()
