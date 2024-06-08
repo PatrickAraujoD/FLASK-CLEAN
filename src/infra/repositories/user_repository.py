815c8a51eedefe5ec855d82efeb7073b52490407
PatrickAraujoD <135553025+PatrickAraujoD@users.noreply.github.com>
2024-06-08 17:40:46 -0300

    refactor: change to add selectUser method

--- src/infra/repositories/user_repository.py
+++ src/infra/repositories/user_repository.py
@@ -1,3 +1,4 @@
+from typing import List
 from src.domain.models import Users
 from src.infra.config import BDConnectionHandler
 from src.infra.entities import Users as UsersModel
@@ -19,3 +20,35 @@ class UserRepository:
             finally:
                 connection.session.close()
         return None
+
+    @classmethod
+    def selectUser(cls, userId: int = None, name: str = None) -> List[Users]:
+        try:
+            query_data = None
+            with BDConnectionHandler() as connection:
+                if userId and not name:
+                    data = (
+                        connection.session.query(UsersModel).filter_by(id=userId).one()
+                    )
+                    query_data = [data]
+
+                elif name and not userId:
+                    data = (
+                        connection.session.query(UsersModel).filter_by(name=name).one()
+                    )
+                    query_data = [data]
+
+                elif userId and name:
+                    data = (
+                        connection.session.query(UsersModel)
+                        .filter_by(id=userId, name=name)
+                        .one()
+                    )
+                    query_data = [data]
+
+                return query_data
+        except:
+            connection.session.close()
+            raise
+        finally:
+            connection.session.close()
